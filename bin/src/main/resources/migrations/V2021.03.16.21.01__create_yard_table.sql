b643e2e14b97f8ec7d71476a9561d206a6909730
Francisco Beltran <fjb.pacho@gmail.com>
2021-05-31 20:59:20 -0500

    Delete V2021.03.16.21.01__create_yard_table.sql

--- bin/src/main/resources/migrations/V2021.03.16.21.01__create_yard_table.sql
+++
@@ -1,8 +0,0 @@
--- Create initial tables.
-
-CREATE TABLE IF NOT EXISTS public.yard (
-          Id serial not null,
-          Color varchar,
-          Warehouse varchar,
-          constraint "PK_Yard" primary key(ID)
-);
